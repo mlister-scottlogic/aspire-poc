0b0f05be7cbefbce0d14128ab654c809f0905dc3
Mark Lister <mlister@scottlogic.com>
2025-07-29 14:29:43 +0100

    Add common project and optional

---
+++ AspireApp/AspireApp.ApiService.Common/Optional.cs
@@ -0,0 +1,51 @@
+﻿namespace AspireApp.ApiService.Common
+{
+    public readonly struct Optional<T>
+    {
+        private readonly T? value;
+
+        private Optional(T? value)
+        {
+            this.value = value;
+        }
+
+        public static Optional<T> Some(T value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            return new Optional<T>(value);
+        }
+
+        public static Optional<T> None()
+        {
+            return new Optional<T>(default);
+        }
+
+        public readonly TResult Match<TResult>(Func<T, TResult> success, Func<TResult> failure)
+        {
+            if (value is not null)
+            {
+                return success(value);
+            }
+            else
+            {
+                return failure();
+            }
+        }
+
+        public async readonly Task<TResult> MatchAsync<TResult>(Func<T, Task<TResult>> success, Func<Task<TResult>> failure)
+        {
+            if (value is not null)
+            {
+                return await success(value);
+            }
+            else
+            {
+                return await failure();
+            }
+        }
+    }
+}
