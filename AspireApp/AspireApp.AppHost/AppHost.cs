fd3471ab4c9d768bedd139bbdd692f9c1bf76fc0
Mark Lister <mlister@scottlogic.com>
2025-08-04 10:47:46 +0100

    Add rabbitmq dependency

--- AspireApp/AspireApp.AppHost/AppHost.cs
+++ AspireApp/AspireApp.AppHost/AppHost.cs
@@ -3,11 +3,15 @@ var builder = DistributedApplication.CreateBuilder(args);
 var postgres = builder.AddPostgres("postgres", port: 5432);
 var postgresdb = postgres.AddDatabase("postgresdb");
 
+var rabbitmq = builder.AddRabbitMQ("messaging");
+
 var apiService = builder
     .AddProject<Projects.AspireApp_ApiService>("apiservice")
     .WithHttpHealthCheck("/health")
     .WithReference(postgresdb)
-    .WaitFor(postgresdb);
+    .WithReference(rabbitmq)
+    .WaitFor(postgresdb)
+    .WaitFor(rabbitmq);
 
 builder
     .AddProject<Projects.AspireApp_Web>("webfrontend")
